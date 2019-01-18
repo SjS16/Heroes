434438f2723ed0a87e9e9370f38934ac510d24c8
SjS16 <stephanie.simpson@winedirect.com>
2019-01-18 10:56:01 -0800

    added mock heroes to list

---
+++ app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{ id: 11, name: 'Mr. Nice' },
+	{ id: 12, name: 'Mr. Fairly Nice' },
+	{ id: 13, name: 'Banes Good Twin' },
+	{ id: 14, name: 'Bob Ross' },
+	{ id: 15, name: 'Batman' },
+	{ id: 16, name: 'Loki on a Good Day' },
+	{ id: 17, name: 'Superwoman' },
+	{ id: 18, name: 'Wolverine' },
+	{ id: 19, name: 'David Beckhams Right Foot' },
+	{ id: 20, name: 'Spiderman' },
+	{ id: 21, name: 'Jimmy' },
+	{ id: 22, name: 'Dexter' }
+]
