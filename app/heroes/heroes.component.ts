5943a90627174132f246247ac37e962573937914
SjS16 <stephanie.simpson@winedirect.com>
2019-01-18 13:23:21 -0800

    added message service and component

--- app/heroes/heroes.component.ts
+++ app/heroes/heroes.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+ 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-
+ 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
- 
 export class HeroesComponent implements OnInit {
  
-  selectedHero: Hero
-  ;
+  selectedHero: Hero;
+ 
   heroes: Hero[];
  
   constructor(private heroService: HeroService) { }
@@ -23,10 +23,9 @@ export class HeroesComponent implements OnInit {
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  
+ 
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
-  
 }
