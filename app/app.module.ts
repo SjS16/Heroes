3b8da578e3d86d572ae7f176d9066b3bbfd53548
SjS16 <stephanie.simpson@winedirect.com>
2019-01-21 09:59:55 -0800

    added search functionality

--- app/app.module.ts
+++ app/app.module.ts
@@ -13,6 +13,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http'
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
  
 @NgModule({
   imports: [
@@ -32,7 +33,8 @@ import { InMemoryDataService }  from './in-memory-data.service';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   bootstrap: [ AppComponent ]
 })
